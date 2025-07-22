ed9361ccd252c8c172141cba0823554f363cedf7
guilherme <guilherme30110922@gmail.com>
2025-07-21 23:36:53 -0300

    estilizacao

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from './componentes/Header.jsx'
-import Main from './componentes/Main.jsx';
-import Footer from './componentes/Footer.jsx';
+import Header from "./componentes/Header.jsx";
+
+import Main from "./componentes/Main.jsx";
+import Footer from "./componentes/Footer.jsx";
+
 export default function Home() {
   return (
     <div className={styles.page}>
-     <Header/>
-     <Main/>
-     <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
