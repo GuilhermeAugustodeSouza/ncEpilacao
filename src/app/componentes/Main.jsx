f7f4288d8416f3e083d2fccb8302e5d2ac8a7b94
guilherme <guilherme30110922@gmail.com>
2025-07-22 14:04:20 -0300

    alteracoes update

--- src/app/componentes/Main.jsx
+++ src/app/componentes/Main.jsx
@@ -6,6 +6,8 @@ import imagem1 from "./../../../public/1752859044108.jpg";
 import imagem2 from "./../../../public/download (1).jpg";
 import imagem3 from "./../../../public/Full body waxing_.jpg";
 import imagem4 from "./../../../public/ceraQuente.jpg";
+import imagem5 from "./../../../public/22122cf76f986cd49ff6b9375ef72208.jpg";
+import imagem6 from "./../../../public/images.jpg";
 import Link from 'next/link';
 export default function Main() {
 
@@ -68,6 +70,29 @@ export default function Main() {
           </p>
           <button onClick={botaoWhatsapp}>Whatsapp</button>
         </div>
+
+        <div className={estilos.card}>
+          <Image src={imagem5} alt={"Agente Epiladora"} />
+          <h2>Pele lisa, toque de seda!</h2>
+          <p>
+            {" "}
+           Resultados duradouros e menos agressão à sua pele.
+           Experimente uma epilação delicada e eficaz, pensada para você.
+          </p>
+          <button onClick={botaoWhatsapp}>Whatsapp</button>
+        </div>
+
+        <div className={estilos.card}>
+          <Image src={imagem6} alt={"Agente Epiladora"} />
+          <h2>Livre de pelos, livre para brilhar!</h2>
+          <p>
+            {" "}
+           Agende sua sessão e sinta a diferença desde o primeiro toque.
+          Epilação profissional com carinho, respeito e higiene.
+          </p>
+          <button onClick={botaoWhatsapp}>Whatsapp</button>
+        </div>
+
 
       </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import estilos from "./page.module.css";
 import Header from "./componentes/Header.jsx";
-
+ 
 import Main from "./componentes/Main.jsx";
 import Footer from "./componentes/Footer.jsx";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={estilos.page}>
       <Header />
       <Main />
       <Footer />
